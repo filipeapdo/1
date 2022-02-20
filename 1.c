809d1203911c8fa95eb5defdec4f9776f46bc770
filipeapdo <filipeaparecido@gmail.com>
2022-02-20 10:50:10 -0300

    adding .gitignore

--- 1.c
+++ 1.c
@@ -1,14 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   1.c                                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fiaparec <fiaparec@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/20 10:45:16 by fiaparec          #+#    #+#             */
+/*   Updated: 2022/02/20 10:46:55 by fiaparec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	int	i;
 
 	i = 0;
-	printf("\n\nargv = %d\n", argc);
+	printf("\n\nargc = %d\n", argc);
 	while (i < argc)
 	{
-		printf("argv[%d] = %s", i, *(argv + i));
+		printf("argv[%d] = %s\n", i, *(argv + i));
 		i++;
 	}
 	printf("\n\n");
